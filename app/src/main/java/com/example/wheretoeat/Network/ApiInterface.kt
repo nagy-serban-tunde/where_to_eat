b5f09dfa14a27867edf6f7d4e14933c58edb9cc6
nagyserbantunde <nagyserbantunde@gmail.com>
2020-12-02 09:59:56 +0200

    navigation and splash

---
+++ app/src/main/java/com/example/wheretoeat/Network/ApiInterface.kt
@@ -0,0 +1,9 @@
+package com.example.wheretoeat.Network
+
+//import retrofit2.Call
+//import retrofit2.http.GET
+
+interface ApiInterface {
+//    @GET("posts")
+//    fun fetchAllPosts(): Call<List<PostModel>>
+}
